29e219d462192865177231573c97eff75e07bac2
PyDon911029 <jym911029@gmail.com>
2023-07-04 18:33:11 +0900

    Update

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const mongoose = require('mongoose');
 
 var app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,6 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
